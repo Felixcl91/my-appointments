df9fc77d0c5c8679fd9bcf816bbcb8f480234ce5
felix <felix.cruz@appogeodigital.com>
2020-09-04 12:28:52 +0200

    add use Aut on FirebaseController

--- app/Http/Controllers/Api/FirebaseController.php
+++ app/Http/Controllers/Api/FirebaseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+
 class FirebaseController extends Controller
 {
     public function postToken(Request $request) {
@@ -12,7 +14,7 @@ class FirebaseController extends Controller
     	// $rquest->validate($rules);
 
     	$user = Auth::guard('api')->user();
-    	
+
     	if ($request->has('device_token')) {
     		$user->device_token = $request->input('device_token');
     		$user->save();
